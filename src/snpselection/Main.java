a368313059af4ec16466a14fc7fd1ef2693c61a2
Jay Khurana <Jay Khurana@JAYKHURANA-XPS.twcny.rr.com>
2017-01-14 12:00:33 -0500

    Basic framework working, todo list

--- src/snpselection/Main.java
+++ src/snpselection/Main.java
@@ -5,6 +5,16 @@
  */
 package snpselection;
 
+import java.io.BufferedReader;
+import java.util.*;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 /**
  *
  * @author Jay Khurana
@@ -17,6 +27,21 @@ public class Main extends javax.swing.JFrame {
     public Main() {
         initComponents();
     }
+    /**
+     * TODO: Load data
+     * TODO: Add ability to run data and re-enable stuff
+     * TODO: User-hints and tooltip texts
+     * TODO: Error checking for data input and algorithm parameters
+     * TODO: Algorithm-specific parameters
+     * TODO: Refactor GUI elements with better names
+     * TODO: Better dynamic resizing of GUI
+     * Long term TODO:
+     * TODO: Add metadata to data (ex. SNP name, other annotations)
+     * TODO: Add support for retrieving SNPSelection data from thousand genomes problem autonomously with annotations
+     * TODO: Add support for external SNP visualizatio software like UCSC genome browser
+     * TODO: add support for running from command line
+     */
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -56,10 +81,20 @@ public class Main extends javax.swing.JFrame {
 
         jButton1.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
         jButton1.setText("Import Data");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton3.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
         jButton3.setText("Run Data");
         jButton3.setEnabled(false);
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
         jButton4.setText("Save Results");
@@ -165,6 +200,50 @@ public class Main extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     /**
+     * Loads and validates data.
+     * If the data is valid, the rest of the GUI is enabled otherwise an error message is displayed
+     * TODO: Display error message on invalid data
+     * @param evt the event from pressing the import data button
+     */
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String text = retrieveText();
+        try{
+             formatData(text);
+        }catch(Exception exe){
+            //Display error message based on type of exception
+            enableGUI(false);
+            return;
+        }
+        this.jTextField1.setText(""+data.get(0).size());
+        this.jTextField2.setText(""+data.size());
+        enableGUI(true);
+        System.out.println();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+     * Run the chosen user algorithm with the chosen parameters. The algorithm should run on a separate thread/
+     * TODO: add thread that updates user on status of job and blocks other jobs from being started unless the current job is canceled.
+     * @param evt run button press 
+     */
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        int numSNPs = Integer.parseInt(jTextField1.getText());
+        int numHaps = Integer.parseInt(jTextField2.getText());
+        if(this.jRadioButton1.isSelected()){
+            SNPSelectionAlgo algo = new LDSelect(data, metadata, numSNPs, numHaps);
+            try{
+                algo.runAlgo();
+            }catch(Exception exe){
+                //TODO: Analyze exception and notify user
+            }
+        }else if(this.jRadioButton1.isSelected()){
+            //TODO: run HLL algorithm
+        }else{
+            //run tree join algorithm
+        }
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
@@ -199,6 +278,91 @@ public class Main extends javax.swing.JFrame {
         });
     }
 
+    /**
+     * Retrieves text (as String) from file selected by user in file chooser
+     * @return the text from the file chosen as a String
+     */
+    private static String retrieveText(){
+        JFileChooser selectBatch = new JFileChooser();
+        FileFilter textOnly = new FileNameExtensionFilter("Format file", "FORMAT");
+        selectBatch.setFileFilter(textOnly);
+        int fileChosen=selectBatch.showDialog(null,"Open File");
+        StringBuilder text = new StringBuilder();
+        String fileText="";
+        if(fileChosen==1){
+            return null;
+        }
+        BufferedReader fileReader; 
+        Charset charset = Charset.forName("Cp1252");
+        try{    
+            fileReader = Files.newBufferedReader(selectBatch.getSelectedFile().toPath(), charset);
+        }catch(IOException exe){
+            JOptionPane.showMessageDialog(null,"Could not find file at specified"
+                    + " location!","Warning",JOptionPane.WARNING_MESSAGE);
+            return null;
+        }
+        String line;
+        try{    
+            while ((line = fileReader.readLine()) != null) {
+                text=text.append(line).append("\r");
+            }
+        }catch(IOException exe){             
+            JOptionPane.showMessageDialog(null,"Could not read from file at "
+                    + "selected location!","Warning",JOptionPane.WARNING_MESSAGE);        
+        }         
+        fileText=text.toString();
+        return fileText;
+    }
+    
+    /**
+     * Formats data into
+     * TODO: Add exception that is thrown if data is not formatted properly
+     * TODO: Add error checking
+     * TODO (long term) add support for different formats of data
+     * TODO (long term) get metadata from different formats of data
+     */
+    private static void formatData(String text) throws Exception{
+        List<List<Integer>> hapList = new ArrayList<>();
+        Scanner stringReader = new Scanner(text.substring(text.indexOf("\r")+1));
+        while(stringReader.hasNextLine()){
+            String hap = stringReader.nextLine();
+            String [] genotype = hap.split("\t");
+            List<Integer> hap1 = new ArrayList<>();
+            List<Integer> hap2 = new ArrayList<>();
+            for(int i=2; i < genotype.length; i++){
+                try{
+                    String h1SNP = genotype[i].substring(0, genotype[i].indexOf("|"));
+                    int end = genotype[i].lastIndexOf("|") == -1 || genotype[i].lastIndexOf("|") == genotype[i].indexOf("|") ? genotype[i].length() : genotype[i].lastIndexOf("|");
+                    String h2SNP = genotype[i].substring(genotype[i].indexOf("|")+1, end);                    
+                    hap1.add(Integer.parseInt(h1SNP));
+                    hap2.add(Integer.parseInt(h2SNP));
+                }catch(Exception exe){
+                    throw new NumberFormatException();
+                }
+            }
+            if(hap1.size() != hap2.size() || ( hapList.size() >= 2 && hap2.size() != hapList.get(0).size() )){
+                throw new Exception(); //TODO more specific exception
+            }
+            hapList.add(hap1);
+            hapList.add(hap2);
+        }
+        data = hapList;
+        stringReader.close();
+    }
+    
+    private void enableGUI(boolean enable) {
+        jButton3.setEnabled(enable);
+        jButton4.setEnabled(enable);
+        jRadioButton1.setEnabled(enable);        
+        jRadioButton2.setEnabled(enable);
+        jRadioButton3.setEnabled(enable);
+        jTextField1.setEnabled(enable);
+        jTextField2.setEnabled(enable);
+    }
+    
+    private static List<List<Integer>> data; //SNP data
+    private static HashMap<String, Object> metadata; //metadata containing SNPData, TODO: instead of extending object add support for generic insertion
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JButton jButton1;
@@ -217,4 +381,5 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
+
 }
