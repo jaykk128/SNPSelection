17ad3509d5135bdea9c6a5f8068e74278721cc44
Jay Khurana <Jay Khurana@JAYKHURANA-XPS.twcny.rr.com>
2017-01-14 06:46:32 -0500

    Initial GUI

---
+++ src/snpselection/SNP_Selection_Algorithm.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snpselection;
+
+/**
+ *
+ * @author Jay Khurana
+ */
+public interface SNP_Selection_Algorithm {
+    
+}
